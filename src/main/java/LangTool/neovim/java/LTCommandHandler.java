b1a605351a7642d0c9ec5224de90f9f596434831
vigoux <tomvig38@gmail.com>
2019-09-28 11:15:52 +0200

    feat: hover errors
    In the near future this will open a floatting window with informations
    about the error.

--- src/main/java/LangTool/neovim/java/LTCommandHandler.java
+++ src/main/java/LangTool/neovim/java/LTCommandHandler.java
@@ -13,7 +13,9 @@ import org.slf4j.LoggerFactory;
 
 import com.ensarsarajcic.neovim.java.api.NeovimApi;
 import com.ensarsarajcic.neovim.java.api.buffer.NeovimBufferApi;
+import com.ensarsarajcic.neovim.java.api.types.api.VimCoords;
 import com.ensarsarajcic.neovim.java.api.types.apiinfo.ApiInfo;
+import com.ensarsarajcic.neovim.java.api.window.NeovimWindowApi;
 import com.ensarsarajcic.neovim.java.corerpc.message.NotificationMessage;
 import com.ensarsarajcic.neovim.java.handler.annotations.NeovimNotificationHandler;
 
@@ -23,8 +25,11 @@ public class LTCommandHandler {
     private NeovimApi nvimApi;
     private int channelId;
     private JLanguageTool langTool;
-    private HashMap<NeovimBufferApi, List<RuleMatch>> errors;
+    private HashMap<String, List<RuleMatch>> errors;
     private int namespace;
+
+    public static final String CHECK = "Check";
+    public static final String HOVER = "Hover";
 
     public LTCommandHandler(NeovimApi nvimApi) throws InterruptedException, ExecutionException {
         this.nvimApi = nvimApi;
@@ -33,13 +38,23 @@ public class LTCommandHandler {
 
         this.channelId = infos.getChannelId();
 
-        this.nvimApi.executeCommand(
-                "let LanguageToolCheckJava = {-> rpcnotify(" + this.channelId + ", 'LTCommand', 'Check')}");
-
         this.langTool = new JLanguageTool(new BritishEnglish());
 
-        this.errors = new HashMap<NeovimBufferApi, List<RuleMatch>>();
+        this.errors = new HashMap<String, List<RuleMatch>>();
         this.namespace = 0;
+
+        // Register all functions
+        this.registerFunction("LanguageToolCheckJava", LTCommandHandler.CHECK);
+        this.registerFunction("LanguageToolHoverError", LTCommandHandler.HOVER);
+    }
+
+    private void registerFunction(String functionName, String command) {
+        this.nvimApi.executeCommand(
+                String.format(
+                    "let %s = {-> rpcnotify(%d, 'LTCommand', '%s')}",
+                    functionName,
+                    this.channelId,
+                    command));
     }
 
     @NeovimNotificationHandler("LTCommand")
@@ -47,18 +62,27 @@ public class LTCommandHandler {
         List<?> arguments = message.getArguments();
 
         if (arguments.size() < 1){
-            LTCommandHandler.logger.warn("Received and invalid command : %s", message.toString());
+            LTCommandHandler.logger.warn("Received an invalid command : " + message.toString());
         } else {
-            LTCommandHandler.logger.info("Received : %s", message.toString());
+            LTCommandHandler.logger.info("Received : " + message.toString());
 
             /*
              * Main part of the handler, the one which dispatches tasks
              */
 
             switch (arguments.get(0).toString()) {
-                case "Check":
-                    LTCommandHandler.logger.info("Gettting current buffer...");
+                case LTCommandHandler.CHECK:
+                    LTCommandHandler.logger.info("Getting current buffer...");
                     this.nvimApi.getCurrentBuffer().thenAccept(this::checkBuffer);
+                    break;
+                case LTCommandHandler.HOVER:
+                    this.nvimApi.getCurrentWindow().thenAccept(t -> {
+                        try {
+                            hoverError(t);
+                        } catch (InterruptedException | ExecutionException e) {
+                            e.printStackTrace();
+                        }
+                    });
                     break;
                 default:
                     LTCommandHandler.logger.warn("%s is not a valid command.", arguments.get(0));
@@ -66,6 +90,58 @@ public class LTCommandHandler {
         }
     }
 
+    /*
+     * ERROR HOVERING
+     */
+
+    private void hoverError(NeovimWindowApi window) throws InterruptedException, ExecutionException {
+        RuleMatch errorAtPoint = this.errorAtPoint(window);
+
+        if ( errorAtPoint == null ) {
+            return;
+        }
+
+        this.nvimApi.writeToOutput(errorAtPoint.getShortMessage() + "\n");
+    }
+
+    private RuleMatch errorAtPoint(NeovimWindowApi window) throws InterruptedException, ExecutionException {
+        NeovimBufferApi currentBuffer = window.getBuffer().get();
+
+        List<String> lines = currentBuffer.getLines(0, -1, false).get();
+
+        VimCoords cursor = window.getCursor().get();
+
+        String bufferName = currentBuffer.getName().get();
+
+        if ( !this.errors.containsKey(bufferName) ) {
+            LTCommandHandler.logger.warn("No error for buffer " + currentBuffer.getName().get());
+            LTCommandHandler.logger.info(this.errors.toString());
+            return null;
+        }
+
+        LanguageToolLineCol start;
+        LanguageToolLineCol stop;
+
+        for ( RuleMatch rule : this.errors.get(bufferName) ) {
+            start = new LanguageToolLineCol(rule.getFromPos(), lines);
+            stop = new LanguageToolLineCol(rule.getToPos(), lines);
+
+            if ( cursor.getRow() - 1 >= start.getLine() && 
+                    cursor.getCol() >= start.getCol() &&
+                    cursor.getRow() - 1 <= stop.getLine() &&
+                    cursor.getCol() <= stop.getCol() ) {
+                return rule;
+                    }
+        }
+
+        LTCommandHandler.logger.info("No error at " + cursor.toString());
+        return null;
+    }
+
+
+    /*
+     * BUFFER CHECKING
+     */
     private void checkBuffer(NeovimBufferApi buffer) {
         List<RuleMatch> matches;
         try {
@@ -74,11 +150,12 @@ public class LTCommandHandler {
             List<String> lines = buffer.getLines(0, -1, false).get();
             matches = this.langTool.check(String.join("\n", lines));
 
+            this.errors.put(buffer.getName().get(), matches);
+
             for (RuleMatch match : matches) {
                 this.displayError(match, buffer, lines);
             }
 
-            this.errors.put(buffer, matches);
         } catch (IOException e) {
             LTCommandHandler.logger.error("An error occured %s", e);
         } catch (InterruptedException e) {
@@ -94,7 +171,11 @@ public class LTCommandHandler {
 
         // String erroredText = String.join("\n", lines).subSequence(rule.getFromPos(), rule.getToPos()).toString();
 
-        this.addHighlight(buffer,"LanguageToolGrammarError", start, stop);
+        if ( rule.getType() == RuleMatch.Type.UnknownWord ) {
+            this.addHighlight(buffer,"LanguageToolSpellingError", start, stop);
+        } else {
+            this.addHighlight(buffer,"LanguageToolGrammarError", start, stop);
+        }
     }
 
     private void addHighlight(NeovimBufferApi buffer, String hlGroup, LanguageToolLineCol start, LanguageToolLineCol stop) {
